9e27847e452300b67e723ddbb153d9bb4cd75a03
Craig Stroman <cstroman@gmail.com>
2020-07-26 21:36:50 -0400

    Updating user resolvers

--- resolvers/user.js
+++ resolvers/user.js
@@ -3,9 +3,30 @@ import requiresAuth from '../permissions';
 import formatErrors from '../formatErrors';
 import shortid from 'shortid';
 
-shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-');
+require('dotenv').config();
 
-const resolvers = {
+shortid.characters(process.env.SHORT_ID_CHARACTERS);
+
+export default {
+  User: {
+    /**
+     * Get's the teams.
+     *
+     * @param      {Object}  parent       The parent.
+     * @param      {Object}  args         The arguments.
+     * @param      {Object}  models       The models.
+     * @param      {Object}  user         The user.
+     */
+    teams: (parent, args, { models, user }) =>
+      models.sequelize.query(
+        'select * from teams as team join members as member on team.id = member.team_id where member.user_id = ?',
+        {
+          replacements: [user.id],
+          model: models.Team,
+          raw: true,
+        },
+      ),
+  },
   Query: {
     /**
      * Get's the logged in user information.
@@ -15,24 +36,70 @@ const resolvers = {
      * @param      {Object}  user         The user.
      * @param      {Object}  models       The models.
      */
-    me: requiresAuth.createResolver((parent, args, { user, models }) =>
-      models.User.findOne({ where: { id: user.id } }),
-    ),
+    me: requiresAuth.createResolver((parent, args, { user, models }) => {
+      return models.User.findOne({ where: { id: user.id } });
+    }),
   },
   Mutation: {
     login: async (parent, { email, password }, { models, SECRET1, SECRET2 }) => {
       const loginResult = await tryLogin(email, password, models, SECRET1, SECRET2);
+      let result = {};
 
       if (loginResult.ok) {
-        return {
-          ok: loginResult.ok,
-          user: loginResult.user,
-          token: loginResult.token,
-          refreshToken: loginResult.refreshToken,
-        };
+        const userId = loginResult.userInfo.id;
+
+        try {
+          const teams = await models.sequelize.query(
+            'select * from teams as team join members as member on team.id = member.team_id where member.user_id = ?',
+            {
+              replacements: [userId],
+              model: models.Team,
+              raw: true,
+            },
+          );
+
+          const team = teams[0];
+
+          if (team) {
+            const channel = await models.sequelize.query(
+              "select uuid from channels where channels.name = 'general' and channels.team_id = ?",
+              {
+                replacements: [team.id],
+                model: models.Channels,
+                raw: true,
+              },
+            );
+
+            const channelUUID = channel[0][0].uuid;
+
+            result = {
+              ok: loginResult.ok,
+              user: loginResult.userInfo,
+              teamUUID: team.uuid,
+              channelUUID: channelUUID,
+              token: loginResult.token,
+              refreshToken: loginResult.refreshToken,
+            };
+          } else {
+            result = {
+              ok: loginResult.ok,
+              user: loginResult.userInfo,
+              token: loginResult.token,
+              refreshToken: loginResult.refreshToken,
+            };
+          }
+        } catch (err) {
+          console.log('There are no teams.');
+          result = {
+            ok: loginResult.ok,
+            user: loginResult.userInfo,
+            token: loginResult.token,
+            refreshToken: loginResult.refreshToken,
+          };
+        }
       }
 
-      return 0;
+      return result;
     },
     /**
      * Registers a user.
@@ -61,5 +128,3 @@ const resolvers = {
     },
   },
 };
-
-export default resolvers;
