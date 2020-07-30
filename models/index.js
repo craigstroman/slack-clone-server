d7e83030eb34cfc0b8131c699497b0e2ed8fabc2
Craig Stroman <cstroman@gmail.com>
2020-07-29 22:29:02 -0400

    Fixing models

---
+++ models/index.js
@@ -0,0 +1,38 @@
+import Sequelize from 'sequelize';
+
+require('dotenv').config();
+
+const db = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_USER_PASSWORD;
+
+const sequelize = new Sequelize(db, dbUser, dbPassword, {
+  dialect: 'postgres',
+  define: {
+    underscored: true,
+  },
+  logging: false,
+  // logging: function (str) {
+  //   console.log(str);
+  // },
+});
+
+const models = {
+  User: sequelize.import('./user'),
+  Channel: sequelize.import('./channel'),
+  Message: sequelize.import('./message'),
+  Team: sequelize.import('./team'),
+  Member: sequelize.import('./member'),
+  DirectMessage: sequelize.import('./directMessage'),
+};
+
+Object.keys(models).forEach(modelName => {
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+export default models;
---
+++ models/team.js
@@ -0,0 +1,21 @@
+export default (sequelize, DataTypes) => {
+  const Team = sequelize.define('team', {
+    uuid: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+  });
+
+  Team.associate = (models) => {
+    Team.belongsToMany(models.User, {
+      through: 'member',
+      foreignKey: {
+        name: 'teamId',
+        field: 'team_id',
+      },
+    });
+  };
+
+  return Team;
+};
