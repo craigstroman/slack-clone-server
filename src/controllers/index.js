c64fc3366cbe0077f02814434eb1d08e5a3c9549
Craig Stroman <cstroman@gmail.com>
2026-01-29 20:57:52 -0500

    Deleting a old file

--- src/controllers/index.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-export const indexPage = (req, res) => {
-  res.render('index', {
-    title: req.app.locals.title,
-    content: req.app.locals.content,
-    path: req.path,
-  });
-};
