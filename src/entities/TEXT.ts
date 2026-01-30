97119acfad099fdec7f1770b3f31e2617c73420b
Craig Stroman <cstroman@gmail.com>
2026-01-29 20:50:06 -0500

    Creating text entity

---
+++ src/entities/TEXT.ts
@@ -0,0 +1,25 @@
+import { ObjectType, Field } from 'type-graphql';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+} from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class Text extends BaseEntity {
+  @Field(() => String)
+  @CreateDateColumn()
+  created_at = new Date();
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updated_at = new Date();
+
+  @Field(() => String)
+  @Column()
+  text!: string;
+}
