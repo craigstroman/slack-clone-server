d1b6b89fb91e3f6d0c4d879d708b0453383b235a
Craig Stroman <cstroman@gmail.com>
2026-01-30 14:32:43 -0500

    Creating register mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -10,7 +10,41 @@ import {
   Root,
   Query,
 } from 'type-graphql';
+import { MyContext } from 'src/types';
 import { User } from '../entities/USER';
+import { getConnection } from 'typeorm';
+import argon2 from 'argon2';
+
+@InputType()
+class UsernameRegisterInput {
+  @Field()
+  first_name: string;
+  @Field()
+  last_name: string;
+  @Field()
+  email: string;
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
 
 @Resolver(User)
 export class UserResolver {
@@ -18,5 +52,63 @@ export class UserResolver {
   async register(
     @Arg('options') options: UsernameRegisterInput,
     @Ctx() { req, em }: MyContext,
-  ): Promise<UserResponse> {}
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'Username must be greater than 2 characters long.',
+          },
+        ],
+      };
+    }
+
+    if (options.password.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'Password length must be greater than 2 characters long.',
+          },
+        ],
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const result = await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values({
+        first_name: options.first_name,
+        last_name: options.last_name,
+        username: options.username,
+        email: options.email,
+        password: hashedPassword,
+      })
+      .returning('*')
+      .execute();
+
+    const user = result.raw[0];
+
+    try {
+      await em.persistAndFlush(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        return {
+          errors: [
+            {
+              field: 'username',
+              message: 'username already taken',
+            },
+          ],
+        };
+      }
+    }
+
+    req.session.userId = user.id;
+
+    return { user };
+  }
 }
