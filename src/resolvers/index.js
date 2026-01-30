82be3b4b14e36e2b9756c210021c54448d4c679b
Craig Stroman <cstroman@gmail.com>
2026-01-29 20:54:40 -0500

    Deleting old files

--- src/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-import { mergeResolvers } from 'graphql-tools';
-import channelResolver from './channel';
-import messageResolver from './message';
-import teamResolver from './team';
-import userResolver from './user';
-
-const resolvers = [channelResolver, messageResolver, teamResolver, userResolver];
-
-export default mergeResolvers(resolvers);
