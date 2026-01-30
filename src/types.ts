87ba0505e2c700bc8e562d9e19dd57fd92e053d9
Craig Stroman <cstroman@gmail.com>
2026-01-30 14:47:11 -0500

    Updating types configuration

--- src/types.ts
+++ src/types.ts
@@ -1,26 +1,8 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { createUserLoader } from './utils/createUserLoader';
 
-interface Request {
-  session?: Session & Partial<SessionData>;
-  url: string;
-  hostname: string;
-  params: object;
-  headers: object;
-  cookies: object;
-  body: object;
-  is: string;
-  ip: string;
-  method: string;
-  query: object;
-  protocol: string;
-  originalUrl: string;
-  accepts: string | boolean;
-}
-
 export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request;
+  req: Request & { session: Express.Session };
   res: Response;
   userLoader: ReturnType<typeof createUserLoader>;
 };
