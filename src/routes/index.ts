b9ffd54f1c90f6188b8c975dda02b66f3ef8c2ee
Craig Stroman <cstroman@gmail.com>
2026-01-31 14:27:05 -0500

    Updating routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,8 @@ const router = Router();
 
 if (process.env.NODE_ENV === 'development') {
   router.route(/^\/(?!graphql).*/).get(indexPage);
-} else {
-  router.route('*').get(indexPage);
 }
+
+router.route('/').get(indexPage);
 
 export default router;
