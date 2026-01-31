0b601be19bdb1273c99a34bf08759a4aaf14dad4
Craig Stroman <cstroman@gmail.com>
2026-01-31 14:25:25 -0500

    Fixing issue with team migration

--- src/migrations/1769792716606-team.ts
+++ src/migrations/1769792716606-team.ts
@@ -4,13 +4,13 @@ export class Team1769792716606 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `
-                CREATE TABLE IF NOT EXISTS public."user"
+                CREATE TABLE IF NOT EXISTS public."team"
                 (
                     id integer NOT NULL DEFAULT nextval('user_id_seq'::regclass),
                     created_at timestamp without time zone NOT NULL DEFAULT now(),
                     updated_at timestamp without time zone NOT NULL DEFAULT now(),
                     name character varying COLLATE pg_catalog."default" NOT NULL,
-                    owner character varying COLLATE pg_catalog."default" NOT NULL,
+                    owner character varying COLLATE pg_catalog."default" NOT NULL
                 ); 
             `,
     );
