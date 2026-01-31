d957c08120edf470f5fe5c57b7f6003995e2b08c
Craig Stroman <cstroman@gmail.com>
2026-01-31 14:25:38 -0500

    Fixing issue with member migration

--- src/migrations/1769793102504-member.ts
+++ src/migrations/1769793102504-member.ts
@@ -4,11 +4,11 @@ export class Member1769793102504 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `
-                CREATE TABLE IF NOT EXISTS public."user"
+                CREATE TABLE IF NOT EXISTS public."member"
                 (
                     created_at timestamp without time zone NOT NULL DEFAULT now(),
                     updated_at timestamp without time zone NOT NULL DEFAULT now(),
-                    owner character varying COLLATE pg_catalog."default" NOT NULL,
+                    owner character varying COLLATE pg_catalog."default" NOT NULL
                 ); 
             `,
     );
