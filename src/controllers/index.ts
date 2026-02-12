4e6b0ed29b5cefb1e0564b6d4946f0dae9704108
Craig Stroman <cstroman@gmail.com>
2026-02-12 17:47:43 -0500

    Adding types for request and response

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,6 @@
-export const indexPage = (req, res) => {
+import { Request, Response } from 'express';
+
+export const indexPage = (req: Request, res: Response) => {
   res.render('index', {
     title: req.app.locals.title,
     content: req.app.locals.content,
