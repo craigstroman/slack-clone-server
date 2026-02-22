3a47644612339d44a0b39793b7e9d9900b0b94da
Craig Stroman <cstroman@gmail.com>
2026-02-22 17:51:36 -0500

    Moving entities to one file to prevent circular dependency from happening

---
+++ src/entities/index.ts
@@ -0,0 +1,142 @@
+import { ObjectType, Field } from 'type-graphql';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+  OneToMany,
+  ManyToOne,
+} from 'typeorm';
+
+// User Entity
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field(() => String)
+  @Column()
+  first_name!: string;
+
+  @Field(() => String)
+  @Column()
+  last_name!: string;
+
+  @Field(() => String)
+  @Column()
+  phone_number!: string;
+
+  @Field(() => String)
+  @Column({ unique: true })
+  email!: string;
+
+  @Field()
+  @Column({ unique: true })
+  username!: string;
+
+  @Field()
+  @Column()
+  password!: string;
+
+  @OneToMany(() => Team, (team) => team.creator)
+  teams: [];
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt = new Date();
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt = new Date();
+}
+
+//Team entity
+
+@ObjectType()
+@Entity()
+export class Team extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field(() => String)
+  @Column({ unique: true })
+  name!: string;
+
+  @Field(() => Number)
+  @Column()
+  owner!: number;
+
+  @Field(() => Number)
+  @Column()
+  user_id!: number;
+
+  @Field()
+  @ManyToOne(() => User, (user) => user.teams)
+  creator: User;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  created_at = new Date();
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updated_at = new Date();
+}
+
+// Member entity
+
+@ObjectType()
+@Entity()
+export class Member extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field(() => Number)
+  @Column()
+  team!: number;
+
+  @Field(() => Number)
+  @Column()
+  user_id!: number;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  created_at = new Date();
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updated_at = new Date();
+}
+
+// Text entity
+
+@ObjectType()
+@Entity()
+export class Text extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field(() => String)
+  @Column()
+  text!: string;
+
+  @Field(() => Number)
+  @Column()
+  user_id!: number;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  created_at = new Date();
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updated_at = new Date();
+}
