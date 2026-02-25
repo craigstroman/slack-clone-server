6d8d2a88f98227ae0e824aaa19db3a96717574f8
Craig Stroman <cstroman@gmail.com>
2026-02-25 15:56:01 -0500

    Creating user schema

---
+++ src/schemas/user.ts
@@ -0,0 +1,32 @@
+export const User = `
+    type User {
+        id: Int!
+        first_name: String!
+        last_name: String!
+        phone_number: String!
+        email: String!
+        username: String!
+        password: String!
+        created_at: String!
+        updated_at: String!
+    }
+        
+    type Query {
+        me: User!
+    }   
+
+    type RegisterResponse {
+        ok: Boolean!
+        user: User
+        errors: [Error!]
+    }
+
+    type LoginResponse {
+        User
+    }
+
+    type Mutation {
+        register(first_name: String!, last_name: String!, phone_number: String!, username: String!, email: String!, password: String!): RegisterResponse!
+        login(email: String!, password: String!): LoginResponse!
+    }    
+`;
