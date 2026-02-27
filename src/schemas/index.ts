7d54d76a064f186879e094cd1442212fb9d6558e
Craig Stroman <cstroman@gmail.com>
2026-02-27 12:53:28 -0500

    Deleting index schema file

--- src/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import { mergeTypeDefs } from '@graphql-tools/merge';
-import { userSchema } from './user';
-import { teamSchema } from './team';
-
-export const typeDefs = mergeTypeDefs([userSchema, teamSchema]);
